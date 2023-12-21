4f0e357e74f3f1ae69810a69f01482f3b84b0604
rodolfo-ferreira <rodolfo.ferreira@agencian1.com.br>
2023-12-21 13:25:07 -0300

    style: ajuste na vitrine e arrows

--- components/product/ProductShelf.tsx
+++ components/product/ProductShelf.tsx
@@ -50,7 +50,7 @@ function ProductShelf({
         id={id}
         class="container grid grid-cols-[48px_1fr_48px] px-0 sm:px-5"
       >
-        <Slider class="carousel carousel-center sm:carousel-end gap-6 col-span-full row-start-2 row-end-5">
+        <Slider class="carousel carousel-center sm:carousel-end gap-6 col-span-full row-start-2 row-end-5 mx-auto my-0">
           {products?.map((product, index) => (
             <Slider.Item
               index={index}
@@ -69,12 +69,12 @@ function ProductShelf({
 
         <>
           <div class="hidden relative sm:block z-10 col-start-1 row-start-3">
-            <Slider.PrevButton class="btn btn-circle btn-outline absolute right-1/2 bg-base-100">
+            <Slider.PrevButton class="btn btn-circle btn-outline absolute right-0 bg-base-100">
               <Icon size={24} id="ChevronLeft" strokeWidth={3} />
             </Slider.PrevButton>
           </div>
           <div class="hidden relative sm:block z-10 col-start-3 row-start-3">
-            <Slider.NextButton class="btn btn-circle btn-outline absolute left-1/2 bg-base-100">
+            <Slider.NextButton class="btn btn-circle btn-outline absolute left-0 bg-base-100">
               <Icon size={24} id="ChevronRight" strokeWidth={3} />
             </Slider.NextButton>
           </div>
